339e8b96709848b7a7782e449b5fd0617dd8b793
youn <tty7797@naver.com>
2025-07-07 16:24:37 +0900

    start

---
+++ src/main/java/io/github/yountaewoo/fitory/FitoryApplication.java
@@ -0,0 +1,13 @@
+package io.github.yountaewoo.fitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitoryApplication.class, args);
+	}
+
+}
