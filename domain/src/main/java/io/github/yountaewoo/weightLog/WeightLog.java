8834edf20a8bbc8912e2cd54e78b0b8e3ed56b32
youn <tty7797@naver.com>
2025-07-07 18:33:14 +0900

    feat : weightLog 엔티티 생성

---
+++ domain/src/main/java/io/github/yountaewoo/weightLog/WeightLog.java
@@ -0,0 +1,33 @@
+package io.github.yountaewoo.weightLog;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class WeightLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private double weight;
+
+    private LocalDate recordDate;
+
+    public WeightLog(Long userId, double weight, LocalDate recordDate) {
+        this.userId = userId;
+        this.weight = weight;
+        this.recordDate = recordDate;
+    }
+}
