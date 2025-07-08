e9eb935d1b2816fba714d33973c6db89bae33622
youn <tty7797@naver.com>
2025-07-08 18:19:02 +0900

    feat : routine 리포지토리 생성

---
+++ domain/src/main/java/io/github/yountaewoo/routine/RoutineRepository.java
@@ -0,0 +1,6 @@
+package io.github.yountaewoo.routine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
+}
