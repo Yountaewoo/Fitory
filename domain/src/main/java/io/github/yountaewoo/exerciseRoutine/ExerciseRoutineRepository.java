5e25c23ed7194be8bdfb38abd98b0914b3265516
youn <tty7797@naver.com>
2025-07-08 18:13:38 +0900

    feat : exerciseRoutine 리포지토리 생성

---
+++ domain/src/main/java/io/github/yountaewoo/exerciseRoutine/ExerciseRoutineRepository.java
@@ -0,0 +1,6 @@
+package io.github.yountaewoo.exerciseRoutine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExerciseRoutineRepository extends JpaRepository<ExerciseRoutine, Long> {
+}
