af1baa4737624d376ebb93d2f87ae9e4d8dbbc1b
youn <tty7797@naver.com>
2025-07-08 18:19:58 +0900

    feat : routineLog 리포지토리 생성

---
+++ domain/src/main/java/io/github/yountaewoo/routineLog/RoutineLogRepository.java
@@ -0,0 +1,6 @@
+package io.github.yountaewoo.routineLog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutineLogRepository extends JpaRepository<RoutineLog, Long> {
+}
