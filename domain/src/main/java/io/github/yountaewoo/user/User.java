639388368bdb4f510fe92dbe729fdf3522b8fa6f
youn <tty7797@naver.com>
2025-07-20 17:05:15 +0900

    fix : user 테이블 이름 변경(데이터베이스 충돌)

--- domain/src/main/java/io/github/yountaewoo/user/User.java
+++ domain/src/main/java/io/github/yountaewoo/user/User.java
@@ -1,16 +1,14 @@
 package io.github.yountaewoo.user;
 
 import io.github.yountaewoo.Gender;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "users")
 @Entity
 public class User {
 
